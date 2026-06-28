ce667df8314d3517e56dfbe60d434974d3219b6e
ZHJ787 <zhj787@users.noreply.github.com>
2026-06-28 09:49:16 +0000

    v15: http2 突破 - 重新构建 dist

--- nodejs/src/spider/video/_version.js
+++ nodejs/src/spider/video/_version.js
@@ -6,7 +6,7 @@
 */
 class VersionSpider {
     constructor() {
-        this.meta = { key: '_version', name: '🔧版本-diag6', type: 3 };
+        this.meta = { key: '_version', name: '🔧版本-v15', type: 3 };
     }
 }
 
