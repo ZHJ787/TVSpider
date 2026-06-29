a9e89e40898c8ef5ea11ba835c5bbc58380b445b
ZHJ787 <zhj787@users.noreply.github.com>
2026-06-29 10:56:48 +0000

    v33: 重新构建 (去掉 spawnSync)

--- nodejs/src/spider/video/_version.js
+++ nodejs/src/spider/video/_version.js
@@ -6,7 +6,7 @@
 */
 class VersionSpider {
     constructor() {
-        this.meta = { key: '_version', name: '🔧版本-v32', type: 3 };
+        this.meta = { key: '_version', name: '🔧版本-v33', type: 3 };
     }
 }
 
